a37e78cf66638c928ca4a7da05a5e5a36e2a3beb
amit sapte <sapteab2018@gmail.com>
2025-01-18 23:51:48 +0530

    education added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ import { useEffect } from 'react'
 
 function App() {
 
-  
-    useEffect(() => {
-      const menu = document.getElementById("togalbuttonClick");
-  
-      // Check if screen width is 900px or more
+
+  useEffect(() => {
+    const menu = document.getElementById("togalbuttonClick");
+
+    // Check if screen width is 900px or more
+    if (window.innerWidth >= 770) {
+      menu.classList.add('visiable');
+      menu.classList.remove('nonvisiable');
+    } else {
+      menu.classList.add('nonvisiable');
+      menu.classList.remove('visiable');
+    }
+
+    // Optionally, add a resize event listener to adjust when resizing the window
+    const handleResize = () => {
       if (window.innerWidth >= 770) {
         menu.classList.add('visiable');
         menu.classList.remove('nonvisiable');
@@ -20,56 +30,46 @@ function App() {
         menu.classList.add('nonvisiable');
         menu.classList.remove('visiable');
       }
-  
-      // Optionally, add a resize event listener to adjust when resizing the window
-      const handleResize = () => {
-        if (window.innerWidth >= 770) {
-          menu.classList.add('visiable');
-          menu.classList.remove('nonvisiable');
-        } else {
-          menu.classList.add('nonvisiable');
-          menu.classList.remove('visiable');
-        }
-      };
-  
-      window.addEventListener('resize', handleResize);
-  
-      // Cleanup event listener on unmount
-      return () => {
-        window.removeEventListener('resize', handleResize);
-      };
-    }, []); // Empty dependency array to run effect only on mount
-  
-    const toggleMenu = () => {
-      const menu = document.getElementById("togalbuttonClick");
-      menu.classList.toggle('visiable');
     };
-  
-    return (
-      <div>
-        <div className='bg-gray-900'>
-          <div className='flex flex-row-reverse p-1'>
-            <button
-              className='block md:hidden p-2 text-gray-50 hover:text-gray-50 focus:outline-none'
-              onClick={toggleMenu}
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup event listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Empty dependency array to run effect only on mount
+
+  const toggleMenu = () => {
+    const menu = document.getElementById("togalbuttonClick");
+    menu.classList.toggle('visiable');
+  };
+
+  return (
+    <div>
+      <div className='bg-gray-900'>
+        <div className='flex flex-row-reverse p-1'>
+          <button
+            className='block md:hidden p-2 text-gray-50 hover:text-gray-50 focus:outline-none'
+            onClick={toggleMenu}
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='white'
+              viewBox='0 0 10 10'
+              stroke='currentColor'
+              className='w-10 h-10'
             >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='white'
-                viewBox='0 0 10 10'
-                stroke='currentColor'
-                className='w-10 h-10'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth='2'
-                  d='M1 1h8M1 5h8M1 9h8'
-                />
-              </svg>
-            </button>
-          </div>
-          
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth='2'
+                d='M1 1h8M1 5h8M1 9h8'
+              />
+            </svg>
+          </button>
+        </div>
+
         <div className="    pt-2 mr-4 text-white gap-8 " id="togalbuttonClick" >
           <div className="flex flex-row justify-center items-center " >
             <img src={github || null} alt='not found' className=' m-2  w-[35px] h-[30px]  object-cover '></img>
@@ -115,15 +115,15 @@ function App() {
               />
             </circle>
           </svg>
-          <div className='flex flex-col  md:gap-28 gap-0  sm:flex-col md:flex-col  lg:flex-row  '>
+          <div className='flex flex-col  md:gap-28 gap-0  sm:flex-col md:flex-col  lg:flex-row  2  '>
             <div className=" text-white   flex flex-col p-0 rounded-lg transition-transform duration-300 ease-in-out transform hover:scale-105 hover:rotate-3 hover:translate-x-4 hover:translate-y-4">
               <div className="text-[30px] font-bold">Hey 👋, I am</div>
               <div className="text-[38px] font-bold">Amit</div>
               <div className="text-[39px] font-bold">Sapte</div>
               <div className="text-[30px] font-bold">A Full Stack Dev</div>
               <div className="text-[20px] text-gray-500 font-bold">Mumbai Maharashtra </div>
-              <div className='h-[120px]'>
-                <button className="custom-shadow bg-gradient-to-bl mt-4 from-[rgb(132,0,255)] to-[rgb(230,0,255)]  h-[40px] w-full   ">Check Resume</button>
+              <div className='h-auto '>
+                <button className="custom-shadow bg-gradient-to-bl mt-6 from-[rgb(132,0,255)] to-[rgb(230,0,255)]  h-[40px] w-full   ">Check Resume</button>
               </div>
             </div>
 
@@ -143,9 +143,9 @@ function App() {
           </div>
         </div>
       </div>
-      <div className=' h-auto p-2  w-full bg-gray-900  flex flex-col  justify-center sm:flex-col md:flex-col  lg:flex-row md:gap-20 gap-2 '>
-        <div className="flex flex-row  justify-center items-center ">
-          <div className="border border-gray-500 md:gap-4 p-3 bg-gray-900 h-[300px] w-auto flex flex-col items-center justify-center ">
+      <div className=' h-auto p-2  w-full bg-gray-900  flex flex-col  md:m-0  justify-center sm:flex-col md:flex-col  lg:flex-row md:gap-20 gap-2 '>
+        <div className="flex flex-row  justify-center items-center  ">
+          <div className="md:border md:border-gray-500 md:gap-4 gap-2 p-3 bg-gray-900 h-auto w-auto flex flex-col items-center justify-center ">
             <div>
               <label className='text-gray-300 text-[30px]'>Front-End</label>
             </div>
@@ -155,7 +155,7 @@ function App() {
                 <label className='text-gray-400 md:text-[18px] text-[12px] font-bold'>React Js</label>
               </div>
               <div className='border border-gray-300  w-[90px] h-[40px] md:w-[130px] md:h-[50px] flex flex-row justify-center rounded-md gap-1 items-center'>
-                <img src='https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg' className= 'md:h-[40px] md:w-[45px] h-[30px] w-[30px] ' alt='not found'></img>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg' className='md:h-[40px] md:w-[45px] h-[30px] w-[30px] ' alt='not found'></img>
                 <label className='text-gray-400 font-bold md:text-[18px] text-[12px]'>Tailwind</label>
               </div>
             </div>
@@ -172,9 +172,9 @@ function App() {
                 <img src='https://www.w3.org/html/logo/badge/html5-badge-h-solo.png' className='md:h-[40px] md:w-[45px] h-[30px] w-[30px] ' alt='not found'></img>
                 <label className='text-gray-400 font-bold md:text-[18px] text-[12px]'>HTML5</label>
               </div>
-              </div>
-              <div>
-            <div className='border border-gray-300   w-[90px] h-[40px] md:w-[130px] md:h-[50px] flex flex-row justify-center rounded-md md:gap-1 items-center'>
+            </div>
+            <div>
+              <div className='border border-gray-300   w-[90px] h-[40px] md:w-[130px] md:h-[50px] flex flex-row justify-center rounded-md md:gap-1 items-center'>
                 <img src='https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png' className='md:h-[40px] md:w-[45px] h-[30px] w-[30px] ' alt='not found'></img>
                 <label className='text-gray-400 font-bold md:text-[18px] text-[12px]'>Bootstrap</label>
               </div>
@@ -191,12 +191,12 @@ function App() {
         </div>
 
         <div className="flex flex-row mt-0  justify-center items-center ">
-          <div className="   p-3 w-auto  h-[300px]   justify-center items-center shadow-2xl  md:gap-6 gap-1 flex flex-col border border-gray-50 shadow-slate-200 bg-gray-900">
+          <div className="   p-3 w-auto  h-auto md:border md:border-gray-500    justify-center items-center shadow-2xl  md:gap-6 gap-2 flex flex-col  md:shadow-slate-200 bg-gray-900">
             <div className="text-[30px] font-bold text-white ">
               BackEnd
             </div>
-            <div className='flex flex-col md:gap-6 gap-1'>
-              <div className='flex flex-col sm:flex-row md:flex-row lg:flex-row justify-center md:gap-8 gap-1 '>
+            <div className='flex flex-col md:gap-6 gap-6'>
+              <div className='flex flex-col sm:flex-row md:flex-row lg:flex-row justify-center md:gap-8 gap-6 '>
                 <div className='border border-gray-300 flex rowjustify-center items-center w-[90px] h-[30px] md:w-[130px]  md:h-[40px]'>
                   <img src='https://nodejs.org/static/logos/nodejsLight.svg' className='md:w-[60px] md:h-[40px] w-[40px] h-[30px]' alt='not found'></img>
                   <label className=' text-white ml-1 text-[12px] md:text-[20px]' >Node</label>
@@ -208,7 +208,7 @@ function App() {
 
 
               </div>
-              <div className='flex flex-col sm:flex-row  md:flex-row lg:flex-row justify-center gap-1 items-center md:gap-2 '>
+              <div className='flex flex-col sm:flex-row  md:flex-row lg:flex-row justify-center gap-4 items-center md:gap-2 '>
 
                 <div className='border border-gray-300 flex justify-center items-center rowmd:w-[130px] md:h-[40px] w-[90px] h-[30px]'>
                   <img src=' https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg' className='md:w-[30px] md:h-[20px] w-[20px] h-[30px] ' alt='not found'></img>
@@ -217,12 +217,12 @@ function App() {
                 </div>
 
                 <div className='border border-gray-300 flex justify-center items-center row md:w-[130px] md:h-[40px] w-[90px] h-[30px]'>
-                  <img src='https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg'  className='md:w-[30px] md:h-[20px] w-[20px] h-[30px]'  alt='not found'></img>
+                  <img src='https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg' className='md:w-[30px] md:h-[20px] w-[20px] h-[30px]' alt='not found'></img>
                   <label className='text-[12px] md:text-[20px] ml-1  text-white' >MySql</label>
 
                 </div>
                 <div className='border border-gray-300 flex justify-center items-center row md:w-[130px] md:h-[40px] w-[90px] h-[30px]'>
-                  <img src=' https://brijesh.xyz/postman.png'  className='md:w-[30px] md:h-[30px] w-[20px] h-[20px]' alt='not found'></img>
+                  <img src=' https://brijesh.xyz/postman.png' className='md:w-[30px] md:h-[30px] w-[20px] h-[20px]' alt='not found'></img>
                   <label className='text-[12px] md:text-[22px] ml-1 text-white' >Postman</label>
 
                 </div>
@@ -237,7 +237,31 @@ function App() {
           </div>
 
         </div>
-      </div> 
+      </div>
+      <div className='h-full w-ull bg-gray-900 '>
+        <div className=' grid md:grid-flow-col  grid-flow-row  text-white  p-4'>
+          <div className='border h-auto w-auto m-1  border-gray-700 '>
+            <div>Master in compute Application</div>
+            <div>Shri V.D.Deshmukh College of MCA </div>
+          </div>
+          <div className='border h-auto w-auto m-1  border-gray-700'>
+            <h4>Bachelor in compute Application</h4>
+            <h5>College of computer Science and Information Technology  </h5>
+          </div>
+          <div className='border h-auto w-auto m-1  border-gray-700 '>
+            <h4>High School in Science </h4>
+            <h5>High school of wadwani </h5>
+          </div>
+          <div className=' border h-auto w-auto m-1  border-gray-700'>
+            <h4>10 th School</h4>
+            <h5>parimal school of latur </h5>
+          </div>
+        </div>
+        <div className='' id="addres">
+              
+        </div>
+
+      </div>
     </div>
   );
 }
